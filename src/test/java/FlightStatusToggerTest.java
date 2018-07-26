03fea0baa6308de27efb4a1c443a105fb64d0260
pyavchik <pyavchik@gmail.com>
2018-07-26 11:08:45 +0300

    removed unnecessary imports

--- src/test/java/FlightStatusToggerTest.java
+++ src/test/java/FlightStatusToggerTest.java
@@ -1,7 +1,5 @@
 
 import io.trueautomation.client.driver.TrueAutomationDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.remote.MobileCapabilityType;
